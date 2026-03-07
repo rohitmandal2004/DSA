06e44f8a4e1c18124e4af01a2204f1863c179646
Rohit Mandal <rohitmandal0804@gmail.com>
2026-03-07 18:28:23 +0530

    first commit

---
+++ BackTracking/Find_Subset.java
@@ -0,0 +1,20 @@
+public class Find_Subset {
+    public static void findSubset(String str, String ans, int i){
+        // Base case
+        if(i == str.length()){
+            System.out.println(ans);
+            return;
+        }
+
+        // Recursion
+        // Include the character at index i
+        findSubset(str, ans + str.charAt(i), i + 1);
+        // Exclude the character at index i
+        findSubset(str, ans, i + 1);
+    }
+
+    public static void main(String args[]){
+        String str = "abc";
+        findSubset(str, "", 0);
+    }
+}
